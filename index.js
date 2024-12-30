78c1486e23ab57380715d613e4e90c6d1a1cfcec
hayder <hayderbd4290@gmail.com>
2024-12-30 14:40:45 +0600

    connect mongodb and get all menu api created

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.7ya1e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+
+    const menuCollection = client.db("bistroDb").collection("menuCollection");
+    const reviewCollection = client.db("bistroDb").collection("reviewCollection");
+    
+    app.get('/menuItem', async(req, res)=>{
+        const result = await menuCollection.find().toArray()
+        res.send(result)
+    })
+    
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    
+  }
+}
+run().catch(console.dir);
+
+
 
 app.get('/', (req, res)=>{
     res.send('bistro track server running')
