c4cd507f36586e070fb6cc40e36ba1d89fab5c90
hayder <hayderbd4290@gmail.com>
2025-01-03 11:50:21 +0600

    delete cart item api done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000
 
@@ -80,6 +80,20 @@ async function run() {
         return res.send({acknowledged:false})
       }
       const result = await cartCollection.insertOne(cart)
+      res.send(result)
+    })
+
+    app.get('/cart', async(req, res)=>{
+      const email = req.query.email
+      const query = {email}
+      const result = await cartCollection.find(query).toArray()
+      res.send(result)
+    })
+
+    app.get('/cart/:id', async(req, res)=>{
+      const id = req.params.id
+      const query = {_id: new ObjectId(id)}
+      const result = await cartCollection.deleteOne(query)
       res.send(result)
     })
     
